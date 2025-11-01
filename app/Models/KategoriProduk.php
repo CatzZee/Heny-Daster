bffaa762035a152d36f6fd0f60287645d572f2bb
Fadhil Majid Mu'tashim <fadhil.majid.2405356@students.um.ac.id>
2025-11-01 10:36:29 +0700

    Refactor models to include relationships and fillable attributes for DetailTransaksi, KategoriProduk, Produk, Transaksi, and User.

--- app/Models/KategoriProduk.php
+++ app/Models/KategoriProduk.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class KategoriProduk extends Model
 {
-    //
+    use HasFactory;
+
+    // Menentukan nama tabel secara eksplisit
+    protected $table = 'kategori_produks';
+
+    // Kolom yang boleh diisi
+    protected $fillable = ['nama_kategori'];
+
+    /**
+     * Relasi 1-to-M: Satu Kategori memiliki banyak Produk.
+     */
+    public function produks()
+    {
+        // 'id_kategori' adalah foreign key di tabel 'produks'
+        return $this->hasMany(Produk::class, 'id_kategori');
+    }
 }
