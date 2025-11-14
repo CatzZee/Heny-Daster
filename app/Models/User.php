0b0b9d1e10180cd6cdd73019d9e32946c8bbc7e8
unpau <daun.darma@gmail.com>
2025-11-14 09:39:59 +0700

    feat: Add RiwayatTransaksi controller for transaction history management and user-specific filtering

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Transaksi;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasFactory;
 
     protected $fillable = [
         'nama',
